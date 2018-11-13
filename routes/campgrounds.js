9f8b5cce151af1caf0f6b3773600b66f3174789a
Jinyu Chen <jychen1996420@gmail.com>
2018-11-13 05:30:41 +0000

    Added Google Map Feature to Show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,16 @@ var express = require("express"),
     Camp = require("../models/camp.js"),
     middleware = require("../middleware/index.js");
 
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+    provider: 'google',
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+};
+
+var geocoder = NodeGeocoder(options);
 
 router.get("/", function(req, res) {
     console.log("entered");
@@ -18,22 +28,32 @@ router.get("/", function(req, res) {
 });
 router.post("/", middleware.isLoggedIn, function(req, res) {
     if (req.body.name && req.body.image && req.body.price) {
-        var author = { username: req.user.username, id: req.user._id };
-        var newcamp = { name: req.body.name, price: req.body.price, image: req.body.image, description: req.body.description, author: author };
-        Camp.create(newcamp, function(err, newcamp) {
-            if (err) {
-                req.flash("error", err.message);
+
+        geocoder.geocode(req.body.location, function(err, data) {
+            if (err || !data.length) {
+                console.log(err);
+                console.log(data.length);
+                req.flash('error', 'Invalid address');
+                return res.redirect("/campgrounds");
             }
-            else {
-                console.log(newcamp);
-                console.log("camp Added In");
-                req.flash("success", "Camp successfully added in!");
-                res.redirect("/campgrounds");
-            }
+            var lat = data[0].latitude;
+            var lng = data[0].longitude;
+            var location = data[0].formattedAddress;
+            var author = { username: req.user.username, id: req.user._id };
+            var newcamp = { name: req.body.name, price: req.body.price, image: req.body.image, description: req.body.description, author: author, location: location, lat: lat, lng: lng };
+            Camp.create(newcamp, function(err, newcamp) {
+                if (err) {
+                    req.flash("error", err.message);
+                }
+                else {
+                    console.log(newcamp);
+                    console.log("camp Added In");
+                    req.flash("success", "Camp successfully added in!");
+                    res.redirect("/campgrounds");
+                }
+            });
         });
-
     }
-
 });
 router.get("/new", middleware.isLoggedIn, function(req, res) {
     res.render("campgrounds/newcamp");
@@ -66,18 +86,26 @@ router.get("/:id/edit", middleware.isAuthor, function(req, res) {
 });
 
 router.put("/:id", middleware.isAuthor, function(req, res) {
-    Camp.findByIdAndUpdate(req.params.id, req.body.campground, function(err, camp) {
-        if (err || !camp) {
-            req.flash("error", "Camp Not Found");
-            res.redirect("/campgrounds");
+    geocoder.geocode(req.body.location, function(err, data) {
+        if (err || !data.length) {
+            req.flash('error', 'Invalid address');
+            return res.redirect("/campgrounds");
         }
-        else {
-            req.flash("success", "Updated Successfully");
-            res.redirect("/campgrounds/" + req.params.id);
-        }
+        req.body.campground.lat = data[0].latitude;
+        req.body.campground.lng = data[0].longitude;
+        req.body.campground.location = data[0].formattedAddress;
+        Camp.findByIdAndUpdate(req.params.id, req.body.campground, function(err, camp) {
+            if (err || !camp) {
+                req.flash("error", "Camp Not Found");
+                res.redirect("/campgrounds");
+            }
+            else {
+                req.flash("success", "Updated Successfully");
+                res.redirect("/campgrounds/" + req.params.id);
+            }
+        });
     });
 });
-
 router.delete("/:id", middleware.isAuthor, function(req, res) {
     Camp.findByIdAndRemove(req.params.id, function(err) {
         if (err) {
